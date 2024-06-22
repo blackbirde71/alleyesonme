b189b0b9054ee4ad7a2f07fc2cef3bccbc25c1f6
marcel kida <47465692+marcelkida@users.noreply.github.com>
2024-06-22 16:53:21 -0700

    mocked

--- frontend/src/components/DataColumn.jsx
+++ frontend/src/components/DataColumn.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useStore } from "../Store";
+import DataVis from "./DataVis";
 
 export default function DataColumn() {
   const currClass = useStore((state) => state.currClass);
   return (
-    <div className="col-span-5 bg-primary-500 rounded-xl py-5 px-2">
+    <div className="col-span-5 bg-primary-500 rounded-xl py-5 px-5 flex flex-col">
       <div className="text-center text-primary-100 flex justify-center items-center flex-col mb-5 text-xl">
-        <div className="rounded-full overflow-clip w-2/3 mb-5"></div>
         {currClass.name}
+      </div>
+      <div className="grid-cols-2 grid-rows-2 w-full h-max  rounded-xl grow grid gap-5">
+        <DataVis />
+        <DataVis />
+        <DataVis />
+        <DataVis />
       </div>
     </div>
   );
---
+++ frontend/src/components/DataVis.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DataVis() {
+  return (
+    <div className="row-span-1 col-span-1 bg-primary-400 rounded-lg p-5">
+      <h1>Data Visualization</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
