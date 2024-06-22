789e01c6f65692d6fa3b78c5ce5972dcac6b5a69
Wonger <justinswong21@gmail.com>
2024-06-22 15:38:46 -0700

    Added .env file for Hume API key Edited retrieve_expressions.py to utilize own API key

--- backend/retrieve_expressions.py
+++ backend/retrieve_expressions.py
@@ -5,9 +5,13 @@ import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Replace with your actual API key
-API_KEY = "ENTER API KEY HERE"
+API_KEY = os.environ.get("HUME_API_KEY")
 
 # WebSocket connection to Hume AI API
 ws = websocket.WebSocket()
