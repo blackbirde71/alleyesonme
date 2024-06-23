cee1393d1221e96e5b5effd850b3c454764f9ef9
marcel kida <47465692+marcelkida@users.noreply.github.com>
2024-06-23 11:27:25 -0700

    spinner

--- frontend/src/components/CustomWebcam.jsx
+++ frontend/src/components/CustomWebcam.jsx
@@ -11,7 +11,7 @@ export default function CustomWebcam() {
   const API_KEY = "6W8ZAm2iRotx0RTXxlOesphgGBqBaF2RcedVPBQjKjfgQVyZ";
   const endpoint = `wss://api.hume.ai/v0/stream/models?api_key=${API_KEY}`;
   const addBoredom = useStore((state) => state.addBoredom);
-  const addTimesLocked = useStore((state) =>state.addTimesLocked)
+  const addTimesLocked = useStore((state) => state.addTimesLocked);
 
   const webcamRef = useRef(null);
   const [currentEmotion, setCurrentEmotion] = useState("");
@@ -23,6 +23,7 @@ export default function CustomWebcam() {
   const [pdfPath, setPdfPath] = useState(null);
   const [analysis, setAnalysis] = useState(null);
   const [transcriptions, setTranscriptions] = useState(null);
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
   const setPdf = useStore((state) => state.setPdf);
 
   const startRecording = () => {
@@ -125,16 +126,16 @@ export default function CustomWebcam() {
           let goodAvg = sum(scores) / scores.length;
           let badAvg = (sum(badScores) - boredomVal) / (badScores.length - 1);
 
-          let wAvg = goodAvg - goodAvg * (1 - (4 * boredomVal + badAvg) / 2)
-          if (wAvg > .6) return .6
-          return wAvg
+          let wAvg = goodAvg - goodAvg * (1 - (4 * boredomVal + badAvg) / 2);
+          if (wAvg > 0.6) return 0.6;
+          return wAvg;
         };
 
         addBoredom({
           score: chooseScore(),
           time: count * 1000,
         });
-        if (chooseScore() >= .3) {
+        if (chooseScore() >= 0.3) {
           addTimesLocked();
         }
       }
@@ -273,7 +274,7 @@ export default function CustomWebcam() {
       alert("Please upload a PDF and complete a recording session first.");
       return;
     }
-
+    isAnalyzing(true);
     fetch("http://127.0.0.1:5000/analyze", {
       method: "POST",
       headers: {
@@ -285,9 +286,11 @@ export default function CustomWebcam() {
       }),
     })
       .then((response) => {
+        isAnalyzing(false);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
+
         return response.json();
       })
       .then((data) => {
@@ -333,6 +336,27 @@ export default function CustomWebcam() {
         disabled={!pdfPath || !transcriptions}
         className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
       >
+        {isAnalyzing ? (
+          <svg
+            aria-hidden="true"
+            role="status"
+            class="inline w-4 h-4 me-3 text-white animate-spin"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="#E5E7EB"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentColor"
+            />
+          </svg>
+        ) : (
+          <></>
+        )}
         Analyze Content
       </button>
       {analysis && (
